744278c2160b1fe549f84cc0cb28a4a1ac9d89ca
Michael Wildman <mwil406@aucklanduni.ac.nz>
2014-08-16 22:58:11 +1200

    Initial Commit

---
+++ conways-game-of-life/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  conways-game-of-life
+//
+//  Created by Michael Wildman on 16/08/14.
+//  Copyright (c) 2014 Michael Wildman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
