a21fe81c0ca62ecb95789bd56f213513639d4b38
Michael Wildman <mwil406@aucklanduni.ac.nz>
2014-08-16 23:28:44 +1200

    Basic GLUT window and scaffolding

--- conways-game-of-life/main.cpp
+++ conways-game-of-life/main.cpp
@@ -7,12 +7,51 @@
 //
 
 #include <iostream>
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include <GLUT/glut.h>
 
-int main(int argc, const char * argv[])
+const GLint WINDOW_WIDTH = 800;
+const GLint WINDOW_HEIGHT = 600;
+
+// main drawing function
+void display(void)
 {
+    
+}
 
-    // insert code here...
-    std::cout << "Hello, World!\n";
+// initialize the view
+void init(void)
+{
+    glClearColor(1.0, 1.0, 1.0, 0.0); // white
+    
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    
+    GLdouble halfWidth = (GLdouble) WINDOW_WIDTH / 2.0;
+    GLdouble halfHeight = (GLdouble) WINDOW_HEIGHT / 2.0;
+    
+    gluOrtho2D(0, WINDOW_WIDTH, 0, WINDOW_HEIGHT);
+    
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+}
+
+// Main program entry point
+int main(int argc, char * argv[])
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Conway's Game of Life");
+    
+    init(); // initialize the view
+    
+    glutDisplayFunc(display); // draw scene
+    
+    glutMainLoop();
+    
     return 0;
 }
 
